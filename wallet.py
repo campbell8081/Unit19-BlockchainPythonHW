063cfdf135ea23119f20bb28ade9d79edf17201c
campbell8081 <campbell8081@gmail.com>
2021-03-01 22:45:17 -0600

    update

--- wallet.py
+++ wallet.py
@@ -1,5 +1,15 @@
 import subprocess
 import json
+import os
+
+from dotenv import load_dotenv
+
+from bit import key, PrivateKey, PrivateKeyTestnet
+from bit.network from NetworkAPI
+from bit import *
+from web3 import Web3
+from eth_account import Account
+
 
 command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
 
@@ -9,3 +19,9 @@ p_status = p.wait()
 
 keys = json.loads(output)
 print(keys)
+
+from constants import *
+
+mnemonic = os.getenv('MNEMONIC', 'annual police carpet session length teach fee derive shoe sniff outdoor always field win shell')
+print(mnemonic)
+
